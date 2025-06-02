53869d5db6e4e8b0b7e8e2d7977bbb6cedf19fc2
Alex Zhang <alex.lx.zhang@gmail.com>
2025-06-02 14:37:37 -0400

    update conv2d reference with cudnn backend context set

--- problems/pmpp/conv2d_py/reference.py
+++ problems/pmpp/conv2d_py/reference.py
@@ -3,6 +3,21 @@ import torch
 import torch.nn.functional as F
 from task import input_t, output_t
 
+class DisableCuDNNTF32:
+    def __init__(self):
+        self.allow_tf32 = torch.backends.cudnn.allow_tf32
+        self.deterministic = torch.backends.cudnn.deterministic
+        pass
+
+    def __enter__(self):
+        torch.backends.cudnn.allow_tf32 = False
+        torch.backends.cudnn.deterministic = True
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        torch.backends.cudnn.allow_tf32 = self.allow_tf32
+        torch.backends.cudnn.deterministic = self.deterministic
+
 
 def ref_kernel(data: input_t) -> output_t:
     """
@@ -12,16 +27,17 @@ def ref_kernel(data: input_t) -> output_t:
     Returns:
         Output tensor after convolution
     """
-    input_tensor, kernel = data
-    return F.conv2d(
-        input_tensor, 
-        kernel,
+    with DisableCuDNNTF32():
+        input_tensor, kernel = data
+        return F.conv2d(
+            input_tensor, 
+            kernel,
 
-        # No padding and no striding
-        # TODO: Can revisit this in future problems
-        stride=1,
-        padding=0
-    )
+            # No padding and no striding
+            # TODO: Can revisit this in future problems
+            stride=1,
+            padding=0
+        )
 
 
 def generate_input(size: int, kernelsize: int, channels: int, batch: int, seed: int) -> input_t:
